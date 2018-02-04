655377478eac3271ad436b8988eefee9ee03d6c4
Daniel Santibáñez Polanco <dansanti@gmail.com>
2018-02-04 02:42:04 -0300

    [ADD]Inline editor

--- __openerp__.py
+++ __openerp__.py
@@ -2,7 +2,7 @@
 {
     "name": """Global Discount\
     """,
-    'version': '9.0.0.8.2',
+    'version': '9.0.0.8.3',
     'category': 'Account/invoice',
     'sequence': 12,
     'author':  'Daniel Santibáñez Polanco',
